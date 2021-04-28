3d3d0cd9abd7afc0555fa1e2e51915f462427aa2
Gabe Hacker <g.hacksaw@gmail.com>
2021-04-28 15:42:06 -0700

    Additions for modules to install

---
+++ AzureNinja/AzureNinja.ps1
@@ -0,0 +1,5 @@
+# "oh my posh" for terminal customization
+# https://www.youtube.com/watch?v=DhzR7mbFE9I
+# https://www.youtube.com/watch?v=9uMorgBB_RU
+# https://github.com/JanEgilRing/presentations
+
--- Script.ps1
+++ Script.ps1
@@ -4,3 +4,11 @@ $date = get-date
 
 #comment
 write-host $date
+
+install-module -name psreadline
+install-module -name psreleasetools
+    #Need this to make suure it works how you wan it to.
+    #Set-PSReadLineOption -PredictionSource history
+
+Install-Module -name oh-my-posh
+Install-Module -name posh-git
